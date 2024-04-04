c835099f7a51a0d5fa02875fccb557ac6c127013
Francisco Nicolas Hernandorena <fnhernandorena@gmail.com>
2024-04-04 13:52:44 -0300

    dde

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,16 @@
 
 use App\Http\Controllers\WatchController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('watches', WatchController::class);
+Route::middleware(['auth'])->group(function () {
+    Route::resource('watches', WatchController::class);
+});
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
